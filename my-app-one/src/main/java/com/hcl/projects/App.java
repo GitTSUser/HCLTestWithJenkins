5d0aa301a027cb18cbdfebc1a53809864658b97b
kurra.kirankumar09 <kurra.kirankumar09@gmail.com>
2021-11-12 11:16:45 +0530

    Saving my-app-one

---
+++ my-app-one/src/main/java/com/hcl/projects/App.java
@@ -0,0 +1,21 @@
+package com.hcl.projects;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.hcl.projects.beans.Employ;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+
+		AnnotationConfigApplicationContext ioc = new AnnotationConfigApplicationContext("com.hcl.projects");
+
+		Employ employ = ioc.getBean(Employ.class);
+
+		System.out.println(employ);
+
+	}
+}
